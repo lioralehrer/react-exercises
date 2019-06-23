c99e0e838a628d4b4ca81563853b4ba2fb262545
Liora <lioralehrer@gmail.com>
2019-06-23 14:30:05 +0300

    landing page with componnent

--- d1_components/ex4/logic.js
+++ d1_components/ex4/logic.js
@@ -1,4 +1,106 @@
+class LandingPage extends React.Component {
+    render() {
+        return (
+            <div>
+                <NavBar />
+                <MainPage />
+            </div>
+        );
+    }
+}
+class NavBar extends React.Component {
+    render() {
+        return (
+            <ul className="navBar">
+                <li>SERVICES</li>
+                <li>WORK</li>
+                <li className="underline">THE TEAM</li>
+            </ul>
+
+        )
+    }
+}
+class MainPage extends React.Component {
+    render() {
+        return (
+            <div className="mainPage">
+                <Header />
+                <Service1 />
+                <Service2 />
+                <Service3 />
+            </div>
+
+        )
+    }
+}
+class Header extends React.Component {
+    render() {
+        return (
+            <div className="header">
+                <h1> Services</h1>
+                <h3>Our vision is simple. We want to create website and applications with both<br /> high quality and easy-to-access content/ the finishe product will be <br /> totally unique and represent awesomeness/</h3>
+
+            </div>
+        )
+    }
+} class Service1 extends React.Component {
+    render() {
+        return (
+            <div className="service service1">
+                <Circle1/>
+                <div><h3>USER INTERFACE DESIGN</h3><h5>Wiretraming<br />Prototyping<br />Usabilitytesting</h5></div>
+            </div>
+        )
+
+    }
+}
+class Circle1 extends React.Component{
+    render(){
+        return(
+            <div className="circle circle1"></div>
+        );
+    }
+}
+class Service2 extends React.Component {
+    render() {
+        return (
+            <div className="service service2">
+                <Circle2/>
+                <div><h3>CONECT AND IDEAS</h3><h5>Conceptualization<br />Digital Branding<br />Product Startegy</h5> </div>
+            </div>
+        )
+
+    }
+}
+class Circle2 extends React.Component{
+    render(){
+        return(
+            <div className="circle circle2"></div>
+        );
+    }
+}
+class Service3 extends React.Component {
+    render() {
+        return (
+            <div className="service service3">
+                <Circle3/>
+                <div><h3>DESIGN AND BRANDIN</h3>
+                    <h5>Interaction Design<br /> Graphic Design <br /> Identity Design </h5>
+                </div>
+            </div>
+        )
+
+    }
+}
+class Circle3 extends React.Component{
+    render(){
+        return(
+            <div className="circle circle3"></div>
+        );
+    }
+}
+
 ReactDOM.render(
-    <div>Hello World</div>,
+    <LandingPage />,
     document.getElementById("root")
 );
