ac4ccba76a19ef53b6a7fad85a98f856e76875ac
Liora <lioralehrer@gmail.com>
2019-06-26 14:43:03 +0300

    calculator with props

--- d2_jsx/ex7-calculator/logic.js
+++ d2_jsx/ex7-calculator/logic.js
@@ -1,31 +1,45 @@
-var data1 = {
-    title: "Calculate Squared Number",
-    inputNumber: "",
-    result: ""
-}
-var data2 = {
-    title: "Calculate Squared  Root Number",
-    inputNumber: "",
-    result: ""
-}
-var data3 = {
-    title: "Calculate Sigma Number",
-    inputNumber: "",
-    result: ""
-}
-var data4 = {
-    title: "Calculate Heighest Between Squared and Sigma ",
-    inputNumber: "",
-    result: ""
-}
+var count = 0;
 class CalculatorApp extends React.Component {
+    constructor(){
+        super();
+        this.squard = this.squard.bind(this);
+        this.sigma = this.sigma.bind(this);
+        this.heighest = this.heighest.bind(this);
+    }
+    squard(num) {
+        let result = num * num
+        return result;
+    }
+    squardRoot(num){
+        let result = Math.sqrt(num, 2);
+        return  result;
+    }
+    sigma(num){
+        let result = 0;
+        for (let i =1; i<num;i++){
+            result +=i;
+        }
+        return result;
+    }
+    heighest(num){
+        let result = 0;
+        let x = this.squard(num);
+        let y = this.sigma(num);
+        if (x>y){
+            result = x;
+        }else {
+            result = y;
+        }
+        console.log("inside heighest");
+        return result;
+    }
     render() {
         return (
             <div>
-                <Calc />
-                <Calc />
-                <Calc />
-                <Calc />
+                <Calc title="Calculate Square" func={this.squard} />
+                <Calc title="Calculate Square Root" func={this.squardRoot} />
+                <Calc title="Calculate Sigma" func={this.sigma} />
+                <Calc title="Calculate Heighest Between Squard and Sigma" func={this.heighest} />
             </div>
         )
     }
@@ -34,30 +48,26 @@ class CalculatorApp extends React.Component {
 class Calc extends React.Component {
     constructor() {
         super();
-        // this.squard = this.squard.bind(this);
+        this.myId = count++;
+        this.calculate = this.calculate.bind(this);
     }
-    squard(num) {
-        let result = 0;
-        if (num === 0) {
-            result = 1;
-        } else {
-            result = num * num;
-        }
-        document.getElementById("result").innerHTML=result;
-    }
+    calculate() {
+        let input = document.getElementById("inputNum" + this.myId).value;
+        let result = this.props.func(parseInt(input))
+        document.getElementById("result" + this.myId).innerHTML = result;
 
+    }
     render() {
         return (
             <div className="container">
-                <h2>{data1.title}</h2>
+                <h2>{this.props.title}</h2>
                 <div className="place">
                     <label htmlFor="number" >Enter Number:  </label>
-                    <input type="number" name="number" id="inputNum" onMouseLeave={()=>this.squard(this.value)}></input>
+                    <input type="text" id={"inputNum" + this.myId} onBlur={this.calculate}></input>
                     <div className="place">
                         <label htmlFor="result">Result: </label>
-                        <input type="number" name="result"id="result"></input>
+                        <span id={"result" + this.myId} ></span>
                     </div>
-
                 </div>
             </div>
         )
