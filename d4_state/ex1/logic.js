ebb79cd550771abb52bb7ad2b83f4cdc2998df8f
Liora <lioralehrer@gmail.com>
2019-06-30 10:34:21 +0300

    state ex-1

--- d4_state/ex1/logic.js
+++ d4_state/ex1/logic.js
@@ -1,4 +1,33 @@
-ReactDOM.render(
-    <div>Hello World</div>,
-    document.getElementById("root")
-);
+class Counter extends React.Component {
+    constructor() {
+        super();
+        this.randomNum = this.randomNum.bind(this);
+        this.state = {
+            random: Math.floor(Math.random() * 100),
+            color: "black"
+        }
+    }
+    randomNum() {
+        let newRandom = Math.floor(Math.random() * 100);
+        this.setState({
+            random: newRandom,
+            color: newRandom % 2 === 0 ? "blue" : "red"
+        })
+    };
+    render() {
+        return (
+            <div>
+                <button onClick={this.randomNum}>Increase Me</button>
+                <span style={{ color: this.state.color }}>{this.state.random}</span>
+            </div>
+        )
+    }
+}
+
+function render() {
+    ReactDOM.render(
+        <Counter />,
+        document.getElementById("root")
+    );
+}
+render();
