fc6d9724624881bad35865f81c1663e0e36566d5
Liora <lioralehrer@gmail.com>
2019-06-30 16:36:37 +0300

    color state

--- d4_state/ex3-color-state/logic.js
+++ d4_state/ex3-color-state/logic.js
@@ -1,4 +1,34 @@
-ReactDOM.render(
-    <div>Hello World</div>,
-    document.getElementById("root")
-);
+class ColorBox extends React.Component{
+    constructor(){
+        super();
+        this.changeColor = this.changeColor.bind(this);
+        this.state={
+            color : ""
+        };
+    }
+    changeColor(event){
+        let newColor = event.target.innerText;
+        this.setState({
+            color : newColor
+        });
+    }
+    render(){
+        return(
+            <div><h2>Pick a color for your Box: </h2>
+            <button onClick={this.changeColor}>Blue</button>
+            <button onClick={this.changeColor}>green</button>
+            <button onClick={this.changeColor}>Orange</button>
+            <div className={"box "+this.state.color}></div>
+            </div>
+        )
+    }
+}
+
+
+function render(){
+    ReactDOM.render(
+        <ColorBox/>,
+        document.getElementById("root")
+    );
+}
+render();
