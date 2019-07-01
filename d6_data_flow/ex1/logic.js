4c401e8dae05f00f146e7972a70e7c0ef1514c0e
Liora <lioralehrer@gmail.com>
2019-07-01 10:50:15 +0300

    data flow: callback

--- d6_data_flow/ex1/logic.js
+++ d6_data_flow/ex1/logic.js
@@ -1,4 +1,49 @@
-ReactDOM.render(
-    <div>Hello World</div>,
-    document.getElementById("root")
-);
+class App extends React.Component{
+    constructor(){
+        super();
+        this.write = this.write.bind(this);
+        this.state={
+            text : ""
+        }
+    }
+    write(text){
+        this.setState({
+            text : text
+        })
+    }
+    render(){
+        return(
+            <div>
+                <Input handleText={this.write}></Input>
+                <span>{this.state.text}</span>
+            </div>
+        );
+    }
+}
+class Input extends React.Component{
+    constructor(){
+        super();
+        this.handleChange= this.handleChange.bind(this);
+    }
+    handleChange(event){
+        this.props.handleText(event.target.value);
+    }
+    render(){
+        return(
+            <div>
+                <label>Select a user name: </label>
+                <input type="text" onChange={this.handleChange} ></input>
+            </div>
+           
+        )
+    }
+}
+
+
+function render(){
+    ReactDOM.render(
+        <App/>,
+        document.getElementById("root")
+    );
+}
+render();
