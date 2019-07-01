37e1bc7b23676a6dfdfc1e56b81ada17a12149ad
Liora <lioralehrer@gmail.com>
2019-07-01 14:17:18 +0300

    button pass and get color

--- d6_data_flow/ex3/logic.js
+++ d6_data_flow/ex3/logic.js
@@ -1,4 +1,42 @@
-ReactDOM.render(
-    <div>Hello World</div>,
-    document.getElementById("root")
-);
+class App extends React.Component {
+    constructor(props){
+        super(props);
+        this.changeBoxColor = this.changeBoxColor.bind(this);
+        this.state = {
+            color: ""
+        }
+    }
+
+    changeBoxColor(event){
+        this.setState({
+            color: event.target.textContent
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <Button handelBoxColor={this.changeBoxColor} color="blue"></Button>
+                <Button handelBoxColor={this.changeBoxColor} color="green"></Button>
+                <Button handelBoxColor={this.changeBoxColor} color="orange"></Button>
+                <div className={"box " + this.state.color}></div>
+            </div>
+        );
+    }
+}
+class Button extends React.Component{
+    render(){
+        return(
+            <button onClick={this.props.handelBoxColor}>{this.props.color}</button>
+        )
+    }
+}
+
+function render() {
+    ReactDOM.render(
+        <App/>,
+        document.getElementById("root")
+    );
+}
+
+render();
