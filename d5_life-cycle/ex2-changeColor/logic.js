a4a0a749f53c595d59f258fbe307457969ee535b
Liora <lioralehrer@gmail.com>
2019-07-02 13:33:50 +0300

    change shape after 5 times of squere to circle

--- d5_life-cycle/ex2-changeColor/logic.js
+++ d5_life-cycle/ex2-changeColor/logic.js
@@ -1,20 +1,4 @@
 class Box extends React.Component {
-    constructor(){
-        super()
-        console.log("Box constractor");
-    }
-    componentWillMount(){
-        console.log("Box will be mounted");
-    }
-    componentDidMount(){
-        console.log("Box was mounted");
-    }
-    componentWillReceiveProps(nextProps){
-        console.log("Box received props!");
-    }
-    componentDidUpdate(prevProps,prevState){
-        console.log("Box was updated");
-    }
     render() {
         console.log("Box render");
         let boxStyle = {
@@ -23,45 +7,39 @@ class Box extends React.Component {
             padding: 10,
             border: "1px solid black",
             height :500,
-            width :500
-
-             
+            width :500,
+            borderRadius: this.props.shape ? "" : "50%"
         }
-
         return <div style={boxStyle}></div>
     }
 }
-
 class App extends React.Component {
     constructor() {
         super();
-        console.log("App constractor");
         this.state = {
-            isShown: true
+            isShown: true,
+            shape : true
         }
-        
-    }
-    componentWillMount(){
-        console.log("App will be mounted");
-       
     }
     componentWillUnmount(){
         clearInterval(this.interval);
     }
     componentDidMount(){
-        console.log("App was mounted");
-        let counter = 0;
       this.interval = setInterval(()=>{
             this.setState({
                 isShown :!this.state.isShown
-            }) ,counter++;
-        },1000)
-        counter===10? clearInterval(change):"";
+            }) 
+        },1000);
+      setInterval(()=>{
+          this.setState({
+              shape : !this.state.shape
+          })
+      },5000)  
     }
     render() {
         console.log("app render");
         return (
-            <Box isShown={this.state.isShown}>
+            <Box isShown={this.state.isShown} shape={this.state.shape}>
             </Box>
         );
     }
