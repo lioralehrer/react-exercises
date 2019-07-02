7477305f8209c1e13b178dcbcda5f5dd73a7ee06
Liora <lioralehrer@gmail.com>
2019-07-02 21:36:19 +0300

    fix the formating

--- d5_life-cycle/ex5/logic.js
+++ d5_life-cycle/ex5/logic.js
@@ -1,4 +1,30 @@
-ReactDOM.render(
-    <div>Hello World</div>,
-    document.getElementById("root")
-);
+class Ques extends React.Component{
+    render(){
+        return(
+            <div><h2>{this.props.question}</h2>
+                <input  type="radio" name={this.props.name}/>yes
+                <input  type="radio" name={this.props.name}/>No
+            </div>
+        )
+    }
+}
+class App extends React.Component{
+    render(){
+        return(
+            <div>
+                <Ques question="Have you programmed before?" name="one"></Ques>
+                <Ques question="Have you Programmed in Java?" name="two"></Ques>
+                <Ques question="Have you Programmed in Python?" name="three"></Ques>
+            </div>
+        )
+    }
+}
+
+function render(){
+    ReactDOM.render(
+        <App/>,
+        document.getElementById("root")
+    );
+
+}
+render();
